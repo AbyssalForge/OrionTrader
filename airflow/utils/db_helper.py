d83afadc24dcbd7b75e7d2972714b0cde6019592
aurelien <ruide.aurelien@gmail.com>
2026-02-12 00:20:10 +0100

    modify: source vault

--- airflow/utils/db_helper.py
+++ airflow/utils/db_helper.py
@@ -3,6 +3,7 @@ Database helper functions for ETL pipeline
 Centralise la gestion des connexions et sessions SQLAlchemy
 """
 
+from urllib.parse import quote_plus
 from models import get_engine, get_session
 from clients.vault_helper import get_vault
 
@@ -16,13 +17,16 @@ def get_db_engine():
     """
     vault = get_vault()
     db_config = {
-        'host': 'postgres',
-        'port': 5432,
+        'host': vault.get_secret('Database', 'POSTGRES_HOST'),
+        'port': int(vault.get_secret('Database', 'POSTGRES_PORT')),
         'database': vault.get_secret('Database', 'POSTGRES_DB'),
         'user': vault.get_secret('Database', 'POSTGRES_USER'),
         'password': vault.get_secret('Database', 'POSTGRES_PASSWORD')
     }
-    connection_string = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+    connection_string = (
+        f"postgresql://{quote_plus(db_config['user'])}:{quote_plus(db_config['password'])}"
+        f"@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+    )
     return get_engine(connection_string)
 
 
